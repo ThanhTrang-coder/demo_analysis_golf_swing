f2badd1ef392614d307c6d4298f5808998a0b049
TungH <tungh@ichiisoft.com>
2024-01-18 14:22:22 +0700

    Initial Commit

---
+++ Demo0118/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo0118
+//
+//  Created by Tung on 18/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
